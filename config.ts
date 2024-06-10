5cfae29ef497042a6f1331a44e26e6a61aa71408
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-06-10 08:35:53 +0800

    Change video frame to 10 FPS

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
 export const ROS_URL = "ws://microros-pi5:9090"; // Update with your ROS WebSocket URL
-export const VIDEO_FRAME_RATE_MS = 1000 / 15; // 15 FPS
+export const VIDEO_FRAME_RATE_MS = 1000 / 10; // 10 FPS
